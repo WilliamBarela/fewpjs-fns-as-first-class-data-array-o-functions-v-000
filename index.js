a9d8c04551d1382cf6c5e137701a4624635bcfe9
William Barela <william.barela.dev@gmail.com>
2020-01-23 03:03:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const wakeDog = function(dogName, dogBreed){
-  action = `Wake ${dogName} the ${dogBreed}`
+  action = `Wake ${dogName} the ${dogBreed};`
   console.log(action);
+  return action;
 };
 
 var leashDog = function(dogName, dogBreed){
