dd08366d200b027e186912a520338cf7fd0f6c37
William Barela <william.barela.dev@gmail.com>
2020-01-23 03:02:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ var wakeDog = function(dogName, dogBreed){
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-const leashDog = function(dogName, dogBreed){
+var leashDog = function(dogName, dogBreed){
   console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 
-const walkToPark = function(dogName, dogBreed){
+var walkToPark = function(dogName, dogBreed){
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 
-const throwFrisbee = function(dogName, dogBreed){
+var throwFrisbee = function(dogName, dogBreed){
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 
-const walkHome = function(dogName, dogBreed){
+var walkHome = function(dogName, dogBreed){
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
 }
 
-const unleashDog = function(dogName, dogBreed){
+var unleashDog = function(dogName, dogBreed){
   console.log(`Unleash ${dogName} the ${dogBreed}`);
 }
 
